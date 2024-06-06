0fbcb98a79ede81082f2b589d8659206721e8c83
Myrmex <dfusi@hotmail.com>
2024-06-06 08:52:45 +0200

    Services

---
+++ Cadmus.Lon.Services/LonRepositoryProvider.cs
@@ -0,0 +1,75 @@
+﻿using System.Reflection;
+using Cadmus.Core;
+using Cadmus.Core.Config;
+using Cadmus.Core.Storage;
+using Cadmus.Mongo;
+using Cadmus.General.Parts;
+using Cadmus.Philology.Parts;
+using System;
+using Cadmus.Lon.Parts;
+
+namespace Cadmus.Lon.Services;
+
+/// <summary>
+/// Cadmus LON repository provider.
+/// </summary>
+/// <seealso cref="IRepositoryProvider" />
+public sealed class LonRepositoryProvider : IRepositoryProvider
+{
+    private readonly IPartTypeProvider _partTypeProvider;
+
+    /// <summary>
+    /// The connection string.
+    /// </summary>
+    public string ConnectionString { get; set; }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="LonRepositoryProvider"/>
+    /// class.
+    /// </summary>
+    public LonRepositoryProvider()
+    {
+        ConnectionString = "";
+        TagAttributeToTypeMap map = new();
+        map.Add(
+        [
+            // Cadmus.General.Parts
+            typeof(NotePart).GetTypeInfo().Assembly,
+            // Cadmus.Philology.Parts
+            typeof(ApparatusLayerFragment).GetTypeInfo().Assembly,
+            // Cadmus.Lon.Parts
+            typeof(LetterInfoPart).GetTypeInfo().Assembly,
+        ]);
+
+        _partTypeProvider = new StandardPartTypeProvider(map);
+    }
+
+    /// <summary>
+    /// Gets the part type provider.
+    /// </summary>
+    /// <returns>part type provider</returns>
+    public IPartTypeProvider GetPartTypeProvider()
+    {
+        return _partTypeProvider;
+    }
+
+    /// <summary>
+    /// Creates a Cadmus repository.
+    /// </summary>
+    /// <returns>repository</returns>
+    public ICadmusRepository CreateRepository()
+    {
+        // create the repository (no need to use container here)
+        MongoCadmusRepository repository = new(_partTypeProvider,
+                new StandardItemSortKeyBuilder());
+
+        repository.Configure(new MongoCadmusRepositoryOptions
+        {
+            ConnectionString = ConnectionString ??
+            throw new InvalidOperationException(
+                "No connection string set for IRepositoryProvider implementation")
+        });
+
+        return repository;
+    }
+}
