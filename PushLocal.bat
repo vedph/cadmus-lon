5c0084ca690e708c2ee01719ea5c04a5015988be
Myrmex <dfusi@hotmail.com>
2024-06-25 18:28:10 +0200

    Version

--- PushLocal.bat
+++ PushLocal.bat
@@ -2,6 +2,6 @@
 echo PRESS ANY KEY TO INSTALL Cadmus Libraries TO LOCAL NUGET FEED
 echo Remember to generate the up-to-date package.
 c:\exe\nuget add .\Cadmus.Lon.Parts\bin\Debug\Cadmus.Lon.Parts.0.0.2.nupkg -source C:\Projects\_NuGet
-c:\exe\nuget add .\Cadmus.Lon.Services\bin\Debug\Cadmus.Lon.Services.0.0.3.nupkg -source C:\Projects\_NuGet
+c:\exe\nuget add .\Cadmus.Lon.Services\bin\Debug\Cadmus.Lon.Services.0.0.4.nupkg -source C:\Projects\_NuGet
 c:\exe\nuget add .\Cadmus.Seed.Lon.Parts\bin\Debug\Cadmus.Seed.Lon.Parts.0.0.4.nupkg -source C:\Projects\_NuGet
 pause
